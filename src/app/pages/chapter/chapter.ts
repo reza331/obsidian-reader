51c2c91717599c75fb53ef2e45156b2cc50e28fd
reza331 <reza331wn@gmail.com>
2026-06-26 18:17:02 +0330

    select chapters

--- src/app/pages/chapter/chapter.ts
+++ src/app/pages/chapter/chapter.ts
@@ -1,23 +1,55 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ChapterStore } from '../../core/chapter-page-service/chapter-store';
+import { MangaStore } from '../../core/manga-page-service/manga-store';
+import { languages } from '../../content/language-options';
 
 @Component({
   selector: 'app-chapter',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './chapter.html',
   styleUrl: './chapter.css',
 })
 export class Chapter implements OnInit {
 
+  router = inject(Router)
   activatedRoute = inject(ActivatedRoute);
   chapterStore = inject(ChapterStore)
+  mangaStore = inject(MangaStore)
+  isChaptersOpen = signal(false)
+  languages = signal(languages)
+  chapterID = signal('')
 
+  openChapters() {
+    this.isChaptersOpen.set(true)
+  }
+
+  closeChapters() {
+    this.isChaptersOpen.set(false)
+  }
+
+  changeLang(event: any) {
+    this.mangaStore.getChapters(this.mangaStore.manga()?.id as string, event.target.value)
+  }
+
+  chapterSelect(chapterID: string) {
+
+    this.closeChapters();
+    this.router.navigate([
+      '/chapter',
+      this.mangaStore.manga()?.id,
+      chapterID
+    ]);
+    
+  }
 
   ngOnInit(): void {
-     this.activatedRoute.params.subscribe(params => {
+    this.activatedRoute.params.subscribe(params => {
+      this.chapterStore.images.set([])
+      this.chapterID.set(params['chapterID'])
       this.chapterStore.getChapterImages(params['chapterID'])
       this.chapterStore.getScanGroup(params['chapterID'])
+      this.mangaStore.loadManga(params['mangaID']);
     });
   }
 
